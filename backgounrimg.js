aede403d17eb1c27c10beabe2b00bbaa5de0e7d8
wonyoungseong <94332623+wonyoungseong@users.noreply.github.com>
2022-09-11 21:20:08 +0900

    Update backgounrimg.js

--- backgounrimg.js
+++ backgounrimg.js
@@ -2,5 +2,5 @@ const images = ["2.jpg","3.jpg","4.jpg"];
 const chosenImage = images[Math.floor(Math.random()*images.length)]
 const bgImage = document.querySelector('body');
 
-bgImage.style.backgroundImage = `url('./${chosenImage}')`;
+bgImage.style.backgroundImage = `url('${chosenImage}')`;
 // bgImage.style.backgroundSize = "cover";
